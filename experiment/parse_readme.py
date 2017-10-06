e6acf10623a8fcaf2c19beedf5df7683a8795947
Karthik Vegi <karthikvegi@outlook.com>
2017-10-05 22:09:31 -0400

    Create parse_readme.py

---
+++ experiment/parse_readme.py
@@ -0,0 +1,51 @@
+import yaml
+import sys
+from pprint import pprint
+
+def banner(msg):
+    print (79 * '#')
+    print ('#', msg)
+    print (79 * '#')
+
+# variables/counters declaration
+in_yaml = False
+content = []
+counter = 0
+
+filename = sys.argv[1]
+
+# Read input file
+with open(filename, 'r') as f:
+    lines = f.readlines()
+
+all_lines = []
+for line in lines:
+    all_lines.append(line.replace("\n",""))
+
+lines = all_lines
+
+# print(lines)
+
+# Loop through all the lines
+for line in lines:
+    counter = counter + 1
+
+    if not in_yaml and line.startswith('```'):
+        in_yaml = True
+    elif in_yaml and line.startswith("```"):
+        in_yaml = False
+    elif in_yaml:
+        content.append(line)
+
+s = '\n'.join(content)
+
+try:
+
+    d = yaml.load(s)
+    print("The file is in a valid yaml format")
+
+    banner('yaml')
+    print (yaml.dump(d, default_flow_style=False))
+
+except Exception as e:
+    print ("ERROR: The file is not in a valid yaml format")
